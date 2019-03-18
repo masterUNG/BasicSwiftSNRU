a4ccd1982703e44b1f9ac7c5fb1ac5fba00c5945
masterUNG <masterUNG@abc.com>
2019-03-18 14:28:11 +0700

    Start DataType

---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variable
+//การประกาศตัวแปร นะจ้ะ
+
+var myName = "MasterUNG"
+myName = "Doramon"
+
+//Explicit Constant
+let myFriend = "Nopita"
+//myFriend = "Sunako" Error เพราะ myFriend เป็น ค่าคงที่ let
+
